de36cd4cdcd2844766ea681eee9855d6d8d60a6d
Paralez <dragontty644@gmail.com>
2024-08-21 02:28:30 +0700

    Initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+    adapter: adapter({
+      fallback: '200.html' // may differ from host to host
+    })
+  }
 };
+
 export default config;
